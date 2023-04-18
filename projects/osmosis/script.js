e9fade83d68123da6f42db300955e109463a31e7
Odiend1 <ooziecorgi@gmail.com>
2023-04-18 00:47:57 +0000

    Added projects page

---
+++ projects/osmosis/script.js
@@ -0,0 +1,83 @@
+var menu = document.getElementById('menu');
+var codeNum = 0;
+
+if(window.innerWidth < 700){
+  menu.hidden = true;
+}
+
+var planet = document.getElementById('planet');
+
+planet.onclick = function(){
+  window.location.href = "/blog/planets/J1407b/";
+}
+
+var qr = document.getElementById('qr');
+qr.style.width = planet.offsetWidth + "px";
+qr.style.height = planet.offHeight + "px";
+
+qr.onclick = function(){
+  window.location.href = "/qr/";
+}
+
+var canvas = document.getElementById('canvas');
+canvas.style.width = planet.offsetWidth + "px";
+canvas.style.height = planet.offHeight + "px";
+
+canvas.onclick = function(){
+  window.location.href = "/canvas/";
+}
+
+
+planet.remove();
+
+document.onkeydown = function (e) {
+    e = e || window.event;
+  if(e.key == 'ArrowUp'){
+      if(codeNum == 0 || codeNum == 1){
+        codeNum++;
+      }
+    else{
+      codeNum = 0;
+    }
+    }
+  if(e.key == 'ArrowDown'){
+     if(codeNum == 2 || codeNum == 3){
+        codeNum++;
+      }
+    else{
+      codeNum = 0;
+    }
+    }
+    if(e.key == 'ArrowLeft'){
+      if(codeNum == 4 || codeNum == 6){
+        codeNum++;
+      }
+    else{
+      codeNum = 0;
+    }
+    }
+  if(e.key == 'ArrowRight'){
+      if(codeNum == 5 || codeNum == 7){
+        codeNum++;
+      }
+    else{
+      codeNum = 0;
+    }
+  }
+  if(e.key == 'b'){
+      if(codeNum == 8){
+        codeNum++;
+      }
+    else{
+      codeNum = 0;
+    }
+    }
+  if(e.key == 'a'){
+      if(codeNum == 9){
+        window.location.href = window.location.href + "blockbreaker"
+      }
+    else{
+      codeNum = 0;
+    }
+  }
+};
---
+++ projects/script.js
@@ -0,0 +1,82 @@
+var menu = document.getElementById('menu');
+var codeNum = 0;
+
+if(window.innerWidth < 700){
+  menu.hidden = true;
+}
+
+var planet = document.getElementById('planet');
+
+planet.onclick = function(){
+  window.location.href = "/blog/planets/J1407b/";
+}
+
+var ssp = document.getElementById('superscript+');
+ssp.style.width = planet.offsetWidth + "px";
+ssp.style.height = planet.offHeight + "px";
+
+ssp.onclick = function(){
+  window.location.href = "/superscript+/";
+}
+
+var osmosis = document.getElementById('osmosis');
+osmosis.style.width = planet.offsetWidth + "px";
+osmosis.style.height = planet.offHeight + "px";
+
+osmosis.onclick = function(){
+  window.location.href = "./osmosis/"
+}
+
+planet.remove();
+
+document.onkeydown = function (e) {
+    e = e || window.event;
+  if(e.key == 'ArrowUp'){
+      if(codeNum == 0 || codeNum == 1){
+        codeNum++;
+      }
+    else{
+      codeNum = 0;
+    }
+    }
+  if(e.key == 'ArrowDown'){
+     if(codeNum == 2 || codeNum == 3){
+        codeNum++;
+      }
+    else{
+      codeNum = 0;
+    }
+    }
+    if(e.key == 'ArrowLeft'){
+      if(codeNum == 4 || codeNum == 6){
+        codeNum++;
+      }
+    else{
+      codeNum = 0;
+    }
+    }
+  if(e.key == 'ArrowRight'){
+      if(codeNum == 5 || codeNum == 7){
+        codeNum++;
+      }
+    else{
+      codeNum = 0;
+    }
+  }
+  if(e.key == 'b'){
+      if(codeNum == 8){
+        codeNum++;
+      }
+    else{
+      codeNum = 0;
+    }
+    }
+  if(e.key == 'a'){
+      if(codeNum == 9){
+        window.location.href = window.location.href + "blockbreaker"
+      }
+    else{
+      codeNum = 0;
+    }
+  }
+};
